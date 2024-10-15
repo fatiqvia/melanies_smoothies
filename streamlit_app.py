4286f54810cbc7ad8bff41b1237c7456e80b1ca4
fatiqvia <fabiana.marques@iqvia.com>
2024-10-15 11:49:50 +0100

    Update streamlit_app.py
    API fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,7 @@ if ingredients_list:
         
         st.success(f"Your Smoothie is ordered, {name_on_order}! ✅")
 
+# New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
